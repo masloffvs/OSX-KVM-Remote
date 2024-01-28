85437c18d8bcc4dffe05b4065f056f0239cb1fcc
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-01-28 23:07:51 +0300

    I'm temporarily not writing the name of the commits. difficult

--- node-src/workers.js
+++ node-src/workers.js
@@ -2,7 +2,7 @@ const fs = require("node:fs");
 const {logger} = require("./logger");
 module.exports = {
 	workers: function () {
-		fs.watch(process.cwd() + '/disks/', {recursive: true}, function (type, file) {
+		fs.watchFile(process.cwd() + '/disks/', {recursive: true}, function (type, file) {
 			logger.debug(type, file)
 		})
 	}
