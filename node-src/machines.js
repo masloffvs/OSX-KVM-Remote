2107eab8a1f3aa7d1ccbadba2a1fc8d9bbc148f0
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-02-06 17:01:34 +0300

    add 'spawnBootableDiskWithUniqData'

--- node-src/machines.js
+++ node-src/machines.js
@@ -5,6 +5,10 @@ const {NetDeviceWithMultiplyForward} = require("./helpers/NetDevice");
 const path = require("node:path");
 const DiskLink = require("./DiskLink");
 const {PhantomFile} = require("./helpers/PhantomFile");
+const fs = require("node:fs");
+const {createRandomMacOSHDD, MASTER_PLISTS} = require("./utils/serialDisk");
+const {R_OK} = require("constants");
+const {Throwable} = require("./Throwable");
 
 const netDevice = new NetDeviceWithMultiplyForward('net0', [
 	// {
@@ -26,6 +30,55 @@ function spawnDataDrive(diskPath) {
 	)
 }
 
+async function spawnBootableDiskWithUniqData(
+	pathToDisk,
+	materPlist
+) {
+	let optionsOfUniqMacHDD = null
+
+	if (!fs.existsSync(materPlist)) {
+		throw new Throwable(
+			`The plist file was not found at path "${pathToDisk}"`,
+			spawnBootableDiskWithUniqData,
+			{
+				pathToDisk,
+				materPlist
+			}
+		)
+	}
+
+	if (fs.existsSync(pathToDisk)) {
+		throw new Throwable(
+			`The disk along the path "${pathToDisk}" already exists and cannot be overwritten to protect your data`,
+			spawnBootableDiskWithUniqData,
+			{
+				pathToDisk,
+				materPlist
+			}
+		)
+	}
+
+	optionsOfUniqMacHDD = await createRandomMacOSHDD(pathToDisk, materPlist)
+	fs.accessSync(pathToDisk, R_OK)
+
+	const openCoreBoot = new VirtualDrive(
+		Infinity,
+		'qcow2',
+		'OpenCoreBoot',
+		pathToDisk,
+		undefined,
+		'none'
+	);
+
+
+	return {
+		optionsOfUniqMacHDD,
+		openCoreBoot,
+		pathToDisk,
+		materPlist
+	}
+}
+
 const ovmfCodeFile = new PhantomFile({
 	path: path.normalize(process.cwd() + "/OVMF_CODE.fd")
 })
@@ -36,8 +89,21 @@ const ovmfVars1024x768File = new PhantomFile({
 
 module.exports = {
 	Machine: {
-		sonoma(options, diskDrive) {
+		async sonoma(options, diskDrive, bootableDiskDrive) {
+			if (!path.isAbsolute(diskDrive)) {
+				throw new Throwable(
+					"The path to the bootloader disk is not absolute",
+					this.sonoma,
+					{}
+				)
+			}
+
 			const dataDrive = spawnDataDrive(diskDrive)
+
+			const openCoreDisk = await spawnBootableDiskWithUniqData(
+				path.normalize(bootableDiskDrive),
+				MASTER_PLISTS.sonoma
+			)
 
 			const opt = Object.assign({
 				kvm: process.platform === 'linux',
@@ -46,7 +112,7 @@ module.exports = {
 					netDevice
 				],
 				storageDevices: [
-					DiskLink.openCoreBoot.createGhostDrive(),
+					openCoreDisk.openCoreBoot,
 					DiskLink.installSonomaMedia.createGhostDrive(),
 					dataDrive
 				],
@@ -71,8 +137,21 @@ module.exports = {
 			return new QemuOpenCoreRunnable(opt)
 		},
 
-		ventura(options, diskDrive) {
+		async ventura(options, diskDrive, bootableDiskDrive) {
+			if (!path.isAbsolute(diskDrive)) {
+				throw new Throwable(
+					"The path to the bootloader disk is not absolute",
+					this.sonoma,
+					{}
+				)
+			}
+
 			const dataDrive = spawnDataDrive(diskDrive)
+
+			const openCoreDisk = await spawnBootableDiskWithUniqData(
+				path.normalize(bootableDiskDrive),
+				MASTER_PLISTS.sonoma
+			)
 
 			const opt = Object.assign({
 				kvm: process.platform === 'linux',
@@ -82,7 +161,7 @@ module.exports = {
 					netDevice
 				],
 				storageDevices: [
-					DiskLink.openCoreBoot.createGhostDrive(),
+					openCoreDisk.openCoreBoot,
 					DiskLink.installVenturaMedia.createGhostDrive(),
 					dataDrive
 				],
