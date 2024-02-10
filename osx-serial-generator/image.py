da42b4795ea708e9c50a63b06c056f447d27e4db
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-02-11 01:04:53 +0300

    add more comments

--- osx-serial-generator/image.py
+++ osx-serial-generator/image.py
@@ -60,19 +60,53 @@ def imager(img="", cfg=""):
     fish_init()
 
     # Partition disk image in guestfish
-    msg("partition disk image")
+    msg("Partitioning disk image")
+
+    # Initialize GUID Partition Table (GPT)
+    msg("Initialize GUID Partition Table (GPT)")
     fish("part-init", "/dev/sda", "gpt")
+
+    # Add partitions to the disk image
+
+    # Partition 1: EFI partition
+    msg("Adding EFI partition")
     fish("part-add", "/dev/sda", "p", "2048", "300000")
+
+    # Partition 2: OpenCore partition
+    msg("Adding OpenCore partition")
     fish("part-add", "/dev/sda", "p", "302048", "-2048")
+
+    # Set the partition type of the EFI partition to EFI System
+    msg("Setting EFI partition type to EFI System")
     fish("part-set-gpt-type", "/dev/sda", "1", "C12A7328-F81F-11D2-BA4B-00A0C93EC93B")
+
+    # Mark the EFI partition as bootable
+    msg("Marking EFI partition as bootable")
     fish("part-set-bootable", "/dev/sda", "1", "true")
+
+    # Format the EFI partition with FAT32 file system
+    msg("Formatting EFI partition with FAT32 file system")
     fish("mkfs", "vfat", "/dev/sda1", "label:EFI")
+
+    # Format the OpenCore partition with FAT32 file system
+    msg("Formatting OpenCore partition with FAT32 file system")
     fish("mkfs", "vfat", "/dev/sda2", "label:OpenCore")
+
+    # Mount the OpenCore partition
+    msg("Mounting the OpenCore partition")
     fish("mount", "/dev/sda2", "/")
+
+    # Create a directory named ESP on the OpenCore partition
+    msg("Creating a directory named ESP on the OpenCore partition")
     fish("mkdir", "/ESP")
+
+    # Mount the EFI partition
+    msg("Mounting the EFI partition")
     fish("mount", "/dev/sda1", "/ESP")
 
-    # Copy files to disk image
+
+
+# Copy files to disk image
     msg("copy files to disk image")
     cfg_path = os.path.join(WORK, "config.plist")
     if os.path.exists(cfg):
@@ -85,27 +119,58 @@ def imager(img="", cfg=""):
         print(f"ERROR: Config file '{cfg}' does not exist.")
         exit(1)
 
+    # Create necessary directories on the OpenCore partition
+    msg("Creating necessary directories on the OpenCore partition")
     fish("mkdir", "/ESP/EFI")
+
+    msg("Creating OC directory")
     fish("mkdir", "/ESP/EFI/OC")
+
+    msg("Creating Kexts directory")
     fish("mkdir", "/ESP/EFI/OC/Kexts")
+
+    msg("Creating ACPI directory")
     fish("mkdir", "/ESP/EFI/OC/ACPI")
+
+    msg("Creating Resources directory")
     fish("mkdir", "/ESP/EFI/OC/Resources")
+
+    msg("Creating Tools directory")
     fish("mkdir", "/ESP/EFI/OC/Tools")
+
+    # Copy files to the OpenCore partition
+    msg("Copying files to the OpenCore partition")
     fish("copy-in", os.path.join(WORK, "EFI/BOOT"), "/ESP/EFI")
+
+    msg("Copying OpenCore EFI file")
     fish("copy-in", os.path.join(WORK, "EFI/OC/OpenCore.efi"), "/ESP/EFI/OC")
+
+    msg("Copying Drivers directory")
     fish("copy-in", os.path.join(WORK, "EFI/OC/Drivers"), "/ESP/EFI/OC/")
+
+    msg("Copying Kexts directory")
     fish("copy-in", os.path.join(WORK, "EFI/OC/Kexts"), "/ESP/EFI/OC/")
+
+    msg("Copying ACPI directory")
     fish("copy-in", os.path.join(WORK, "EFI/OC/ACPI"), "/ESP/EFI/OC/")
+
+    msg("Copying OC Resources directory")
     fish("copy-in", os.path.join(BASE, "resources/OcBinaryData/Resources"), "/ESP/EFI/OC/")
+
+    msg("Copying OC Tools directory")
     fish("copy-in", os.path.join(WORK, "EFI/OC/Tools"), "/ESP/EFI/OC/")
 
-    # Note :)
-    fish("copy-in", "startup.nsh", "/")
+    msg("Copying config.plist")
     fish("copy-in", os.path.join(WORK, "config.plist"), "/ESP/EFI/OC/")
 
-    fish("find", "/ESP/")
-    fish_fini()
+    msg("Copying startup.nsh")
+    fish("copy-in", "startup.nsh", "/")
 
+    # Find files on the OpenCore partition
+    msg("Finding files on the OpenCore partition")
+    fish("find", "/ESP/")
+
+    fish_fini()
 
 if __name__ == "__main__":
     import argparse
