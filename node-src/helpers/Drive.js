d37597fa4ba12061926d9ce1be8e0a5762e04e4f
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-02-11 01:58:21 +0300

    removal of bodies of functions and classes. splitting

--- node-src/helpers/Drive.js
+++ node-src/helpers/Drive.js
@@ -1,22 +1,38 @@
-const fs = require("node:fs");
-const path_ = require("node:path");
-const crypto = require('node:crypto');
-const {parseQemuDiskInfo} = require("./QemuDiskInfo");
-const md5 = require('md5');
-const {logger} = require("../logger");
-const {Throwable} = require("../Throwable");
+const {VirtualDrive} = require("./VirtualDrive");
+const {Drive} = require("./DriveDefault");
+const {HDD} = require("./HDD");
+const {SSD} = require("./SSD");
+const path = require("node:path");
 
-function generateHardDiskID(length = 12) {
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	let randomString = '';
+// const openCoreBoot = new VirtualDrive(
+// 	Infinity,
+// 	'qcow2',
+// 	'OpenCoreBoot',
+// 	process.cwd() + '/OpenCore/OpenCore.qcow2',
+// 	undefined,
+// 	'none'
+// );
 
-	for (let i = 0; i < length; i++) {
-		const randomIndex = Math.floor(Math.random() * characters.length);
-		randomString += characters.charAt(randomIndex);
-	}
+const installSonomaMedia = new VirtualDrive(
+	Infinity,
+	'raw',
+	'InstallMedia',
+	path.normalize(process.cwd() + '/prebuilt/basesystems/BaseSystem-Sonoma.img'),
+	undefined,
+	'none',
+	false
+);
 
-	return randomString;
-}
+const installVenturaMedia = new VirtualDrive(
+	Infinity,
+	'raw',
+	'InstallMedia',
+	path.normalize(process.cwd() + '/prebuilt/basesystems/BaseSystem-Ventura.img'),
+	undefined,
+	'none',
+	false
+);
+
 
 /**
  * Create a QEMU driver device configuration string in the specified format.
@@ -32,164 +48,13 @@ function driverDevice(device, options = {}) {
 	return `-device ${device}${optionStrings ? `,${optionStrings}` : ''}`;
 }
 
-async function toVirtualDrive(path, if_='none', isSnapshot=false) {
-	const info = await parseQemuDiskInfo(path)
-
-	return new VirtualDrive(
-		Infinity,
-		info.fileFormat,
-		undefined,
-		info.image || path,
-		undefined,
-		if_,
-		isSnapshot
-	)
-}
-
-class Drive {
-	constructor(size, format, label) {
-		this.size = size;
-		this.format = format; // qcow2, raw
-		this.label = label;
-	}
-
-	getDetails() {
-		return `Drive: ${this.label}, Size: ${this.size}GB, Format: ${this.format}`;
-	}
-
-	getQemuDriveConfig() {
-		return `-drive file=${this.label},format=${this.format},size=${this.size}G`;
-	}
-}
-
-class HDD extends Drive {
-	constructor(size, format, label) {
-		super(size, format, label);
-	}
-
-	getQemuDriveConfig() {
-		return `-drive file=${this.label},media=disk,format=${this.format},size=${this.size}G`;
-	}
-}
-
-class SSD extends Drive {
-	constructor(size, format, label) {
-		super(size, format, label);
-	}
-
-	getQemuDriveConfig() {
-		return `-drive file=${this.label},media=disk,format=${this.format},cache=writeback,size=${this.size}G`;
-	}
-}
-
-/**
- * Represents a virtual drive used in a virtual machine configuration.
- * @extends Drive
- */
-class VirtualDrive extends Drive {
-	/**
-	 * Create a new instance of the VirtualDrive class.
-	 * @param {number} size - The size of the virtual drive in bytes.
-	 * @param {string} format - The format of the virtual drive (e.g., 'qcow2', 'raw').
-	 * @param {string} label - The label or name for the virtual drive.
-	 * @param {string} path - The path to the physical file representing the virtual drive.
-	 * @param {string} [id=undefined] - The unique identifier for the virtual drive.
-	 * @param {string} [if_='none'] - The interface type for the virtual drive (e.g., 'ide', 'scsi', 'virtio', 'none').
-	 * @param {boolean} [snapshot=true] - The snapshot mode for the virtual drive (bool).
-	 * @param bootindex
-	 * @param serial
-	 */
-	constructor(
-		size,
-		format,
-		label,
-		path,
-		id = undefined,
-		if_ = 'none',
-		snapshot = true,
-		bootindex = null,
-		serial = generateHardDiskID(20)
-	) {
-		super(size, format, label);
-
-		logger.info(`init VirtualDrive "${label || id || path}"`)
-
-		if (typeof path != 'string') {
-			logger.error(`init VirtualDrive "${label || id || path}"`)
-			throw new Throwable(
-				"Only the string can be specified as the path to the virtual disk file",
-				VirtualDrive
-			)
-		}
-
-		if (path_.isAbsolute(path)) {
-			this.path = path;
-		} else {
-			this.path = path_.normalize(process.cwd() + '/' + path);
-		}
-
-		if (!fs.existsSync(this.path)) {
-			logger.error(`virtual disk at path '${this.path}' not found`)
-			throw new Throwable(
-				"The virtual disk was not created because the virtual disk file was not found at the specified path",
-				VirtualDrive,
-				{classdump: this}
-			)
-		}
-
-		fs.accessSync(this.path); // Ensure the file at 'path' exists
-
-		this.if_ = if_;
-		this.id = id || label || generateHardDiskID();
-		this.snapshot = snapshot ? 'on' : 'off';
-		this.bootindex = bootindex;
-		this.serial = serial;
-
-		if (!['ide', 'scsi', 'virtio', 'none'].includes(if_)) {
-			throw "exit"
-		}
-
-		if (!['qcow2', 'raw'].includes(format)) {
-			throw "exit"
-		}
-	}
-
-	createGhostDrive() {
-		const newPath = path_.normalize(process.cwd() + '/.cache/' + md5(this.path + (new Date()).getTime()))
-
-		fs.copyFileSync(
-			(this.path),
-			newPath
-		)
-
-		this.path = newPath
-
-		return this
-	}
-
-	/**
-	 * Get details about the virtual drive.
-	 * @returns {string} Details of the virtual drive, including size, format, label, and path.
-	 */
-	getDetails() {
-		return `${super.getDetails()}, Path: ${this.path}`;
-	}
-
-	/**
-	 * Get the QEMU drive configuration string for the virtual drive.
-	 * @returns {string} QEMU drive configuration string.
-	 */
-	getQemuDriveConfig() {
-		return `-drive id='${this.id || 'ra'}',format=${this.format},if=${this.if_},snapshot=${this.snapshot},file=${this.path}${this.bootindex ? `,bootindex=${this.bootindex},serial=${this.serial}` : ''}`;
-	}
-}
-
-
 module.exports = {
 	Drive,
 	HDD,
 	SSD,
 	VirtualDrive,
 	driverDevice,
-	toVirtualDrive
+
+	MediaSonoma: installSonomaMedia,
+	MediaVentura: installVenturaMedia,
 }
