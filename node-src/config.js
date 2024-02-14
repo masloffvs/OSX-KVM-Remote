9d091bf93ebe99db89ee1c67ea1ec943e35df217
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-02-15 01:26:47 +0300

    add app config

--- node-src/config.js
+++ node-src/config.js
@@ -1,5 +1,6 @@
 const {readFileSync} = require("node:fs");
 
 const config = JSON.parse(String(readFileSync(process.cwd() + "/hypervisor.json")))
+const app = JSON.parse(String(readFileSync(process.cwd() + "/app.json")))
 
-module.exports = {config}
+module.exports = { config, app }
