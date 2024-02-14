1b207988a8a8d102a3c79209079776a82c06dcb1
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-02-15 00:57:52 +0300

    make fields private

--- node-src/Foundation/AppleComputer.js
+++ node-src/Foundation/AppleComputer.js
@@ -3,34 +3,100 @@ const {NetDeviceWithMultiplyForward} = require("../helpers/NetDevice");
 const {app} = require("../app");
 const _ = require("lodash");
 const QemuOpenCoreRunnable = require("../OpenCoreRunnable");
+const {UnixVirtualization} = require("./UnixVirtualization");
+const {Throwable} = require("../Throwable");
 
-class AppleComputer {
+class AppleComputer extends UnixVirtualization {
 	/**
+	 * @private
+	 * @type {{}}
+	 * @private
+	 */
+	_unixVirtualizationFinalArguments = {}
+
+	/**
+	 * @private
 	 * @type {{host: number, vm: number}[]}
 	 * @private
 	 */
 	_portForwarding = []
+
 	/**
+	 * @private
 	 * @type {AppleVirtualDrive[]}
 	 * @private
 	 */
 	_virtualDevicesDrives = []
+
 	/**
+	 * @private
 	 * @type {AppleVirtualDrive[]}
 	 * @private
 	 */
 	_virtualDataDrives = []
+
 	/**
+	 * @private
 	 * @type {PhantomFile[]}
 	 * @private
 	 */
 	_ovmfFiles = []
+
+	/**
+	 * @private
+	 * @type {[]}
+	 * @private
+	 */
 	_display = []
+
+	/**
+	 * @private
+	 * @type {[]}
+	 * @private
+	 */
 	_vnc = []
+
+	/**
+	 * @private
+	 * @type {[]}
+	 * @private
+	 */
+	_sataPort = []
+
+	/**
+	 * @private
+	 * @type {string}
+	 * @private
+	 */
 	_hypervisorVmx = "hypervisor=off,vmx=on"
 
+	/**
+	 * @private
+	 * @type {number}
+	 * @private
+	 */
+	_ram = 1024
+
+	/**
+	 * @private
+	 * @type {string}
+	 * @private
+	 */
 	_newInterface = 'net0'
+
+	/**
+	 * @private
+	 * @type {boolean}
+	 * @private
+	 */
 	_enableGraphic = true
+
+	/**
+	 * @private
+	 * @type {*}
+	 * @private
+	 */
+	_accel = undefined
 
 	setPortForward(host, vm) {
 		this._portForwarding.push({
@@ -41,8 +107,60 @@ class AppleComputer {
 		return this
 	}
 
-	setDrive(appleVirtualDrive) {
-		this._virtualDevicesDrives.push(appleVirtualDrive)
+	setVersionSystem(osVersion = 'ventura') {
+		if (!['sonoma', 'ventura'].includes(osVersion)) {
+			throw new Throwable(
+				`An unsupported system version type was specified (${osVersion})`,
+				AppleComputer,
+				{ classdump: this }
+			)
+		}
+		this._unixVirtualizationFinalArguments['cpuType'] = String(osVersion).toLowerCase() !== 'sonoma' ? 'Penryn' : 'Haswell-noTSX'
+		return this
+	}
+
+
+	/**
+	 * Apple computers do not have the usual KVM.
+	 * Therefore, the AppleComputer class disables KVM on Apple devices if OSX-KVM is running
+	 * on a Macintosh Host machine.
+	 * But you can use accelerators that are built into Apple - hvf
+	 *
+	 * @param {'hvf'|'kvm'} accel
+	 * @return {AppleComputer}
+	 */
+	useAppleKvm(accel = 'hvf') {
+		this._accel = accel
+
+		return this
+	}
+
+	/**
+	 *
+	 * @param {AppleVirtualDrive} appleVirtualDrive
+	 * @param sataPort
+	 * @return {AppleComputer}
+	 */
+	setDrive(appleVirtualDrive, sataPort = undefined) {
+		if (appleVirtualDrive) {
+			this._virtualDevicesDrives.push(appleVirtualDrive)
+
+			if (sataPort) {
+				return this.useSataPortWithDrive(parseInt(sataPort), appleVirtualDrive._label)
+			}
+		}
+
+		return this
+	}
+
+	setRam(ramSize = 0) {
+		this._ram = ramSize
+
+		return this
+	}
+
+	useSataPortWithDrive(sataNumber = 0, driveName = 'Untitled') {
+		this._sataPort.push({ sataNumber, driveName })
 
 		return this
 	}
@@ -85,6 +203,10 @@ class AppleComputer {
 		return this
 	}
 
+	/**
+	 * @private
+	 * @return {string[]}
+	 */
 	buildDeviceParamsList() {
 		const OSK = 'ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc'
 
@@ -100,9 +222,17 @@ class AppleComputer {
 		]
 	}
 
+	/**
+	 * @private
+	 * @return {{kvm: boolean, networkDevices: NetDeviceWithMultiplyForward[], allocatedRam: string, otherArgs: *[], nographic: boolean, audioDevices: *[], displayDevices: *[], additionalCpuOptions: string}}
+	 */
 	options() {
 		const virtualDrivers =  this._virtualDevicesDrives.map(disk => disk._q())
 		const dataDrive = this._virtualDataDrives.map(disk => disk._q())
+
+		const sataPorts = this._sataPort.map(sata => {
+			return driverDevice('ide-hd', { bus: `sata.${sata.sataNumber}`, drive: sata.driveName })
+		})
 
 		const dataDriveLinkArgs = this._virtualDataDrives
 			.map((i, index) => {
@@ -125,11 +255,14 @@ class AppleComputer {
 					['-smbios', 'type=2']
 				])
 				.concat(dataDriveLinkArgs)
+				.concat(sataPorts)
+				.concat([this._accel ? `-accel ${this._accel}` : undefined])
 				.concat(['-vga vmware', '-monitor unix:qemu-monitor-socket,server,nowait'])
 		)
 
-		return Object.assign({
+		return {
 			kvm: process.platform === 'linux',
+			allocatedRam: String(this._ram),
 			nographic: this._enableGraphic,
 			networkDevices: [
 				new NetDeviceWithMultiplyForward(this._newInterface, this._portForwarding)
@@ -140,15 +273,32 @@ class AppleComputer {
 
 			additionalCpuOptions: `${this._hypervisorVmx},`,
 			otherArgs: args
-		}, {})
+		}
 	}
 
+	/**
+	 * @deprecated
+	 * @return {QemuOpenCoreRunnable}
+	 */
 	spawnQemuRunnable() {
 		return new QemuOpenCoreRunnable(this.options())
 	}
 
-	start() {
+	/**
+	 * @private
+	 * @return {unknown[] | *}
+	 */
+	spawnQemuArguments() {
+		const finalArgs = Object.assign(
+			this.options(),
+			this._unixVirtualizationFinalArguments
+		)
 
+		return UnixVirtualization.buildOptionsToQemuArgsList(finalArgs)
+	}
+
+	spawnAndRunComputer() {
+		return this.run(this.spawnQemuArguments())
 	}
 }
 
