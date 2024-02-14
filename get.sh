b69801d7114f5aca1c1aa508cf4c684b18a42033
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-02-15 01:26:29 +0300

    Add support arch64

--- get.sh
+++ get.sh
@@ -4,8 +4,17 @@
 if ! command -v git &> /dev/null; then
     # Install git based on the operating system
     if [[ "$(uname)" == "Linux" ]]; then
-        echo "[INSTALL_GIT]: Git is not installed. Installing Git via apt..."
-        sudo apt install git -y
+        # Check if pacman is available
+        if command -v pacman &> /dev/null; then
+            echo "[INSTALL_GIT]: Git is not installed. Installing Git via pacman..."
+            sudo pacman -S git --noconfirm
+        elif command -v apt &> /dev/null; then
+            echo "[INSTALL_GIT]: Git is not installed. Installing Git via apt..."
+            sudo apt install git -y
+        else
+            echo "[INSTALL_GIT]: Git is not installed, and the package manager is not recognized. Please install Git manually and try again."
+            exit 1
+        fi
     elif [[ "$(uname)" == "Darwin" ]]; then
         echo "[INSTALL_GIT]: Git is not installed. Installing Git via brew..."
         brew install git
